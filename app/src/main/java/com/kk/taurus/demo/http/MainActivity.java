93b1630f83b97351778e47b486d288af1eacf1d7
jiajunhui <309812983@qq.com>
2017-02-04 17:24:19 +0800

    add some

--- app/src/main/java/com/kk/taurus/demo/http/MainActivity.java
+++ app/src/main/java/com/kk/taurus/demo/http/MainActivity.java
@@ -2,10 +2,13 @@ package com.kk.taurus.demo.http;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.kk.taurus.http_helper.XHTTP;
 import com.kk.taurus.http_helper.bean.XRequest;
 import com.kk.taurus.http_helper.callback.BeanCallBack;
+
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +26,20 @@ public class MainActivity extends AppCompatActivity {
         XHTTP.newGet(xRequest, new BeanCallBack<WxArticleRsp>() {
             @Override
             public void onResponseBean(WxArticleRsp result) {
+                Toast.makeText(MainActivity.this, "response", Toast.LENGTH_SHORT).show();
                 System.out.println("data : " + result);
+            }
+
+            @Override
+            public void onError(Response response) {
+                super.onError(response);
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                super.onFailure(e);
+                Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
             }
         });
 
